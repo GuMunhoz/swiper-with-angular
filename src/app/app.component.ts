0e713bba86ab209926e42cca2f0b17f4b2c8cc33
Gustavo Munhoz Santos <gmunhoz@premiersp.local>
2024-02-05 13:47:24 -0300

    create swipper mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'swiper-test';
+  numbers: number[] = [1,2,3,4,5,6,7,8,9,10];
+
 }
